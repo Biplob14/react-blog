e77937df496705fb6d97898c751334855c0da2db
biplob14 <biploburrahaman@gmail.com>
2021-02-08 01:55:07 +0600

    archive page with routes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The React Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
